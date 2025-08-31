f939b3ec991319393891739c707e6181cb6a09d0
chhong <pnci1029@gmail.com>
2025-09-01 00:28:09 +0900

    [feat] 배포 서버 파일 제거

--- be/src/main/kotlin/io/be/config/SubdomainConfig.kt
+++ be/src/main/kotlin/io/be/config/SubdomainConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 import java.util.regex.Pattern
 
+
 @Configuration
 @EnableConfigurationProperties(SubdomainProperties::class)
 class SubdomainConfig
