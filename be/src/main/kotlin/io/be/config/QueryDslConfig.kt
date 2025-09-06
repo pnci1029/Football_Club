88135892e3e40fefa1efe7e5a78e468b9305a022
chhong <pnci1029@gmail.com>
2025-09-06 13:43:47 +0900

    [feat] 쿼리 DSL 컨피그 구성

---
+++ be/src/main/kotlin/io/be/config/QueryDslConfig.kt
@@ -0,0 +1,17 @@
+package io.be.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueryDslConfig(
+    private val entityManager: EntityManager
+) {
+    
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(entityManager)
+    }
+}
